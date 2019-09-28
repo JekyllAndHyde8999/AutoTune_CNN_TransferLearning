75661224883cc451115af1bf8988aab591e64a36
shabbeersh <36790230+shabbeersh@users.noreply.github.com>
2019-09-28 11:55:48 +0530

    Update Gen_Layers.py

--- Birds200/Gen_Layers.py
+++ Birds200/Gen_Layers.py
@@ -45,7 +45,7 @@ def get_model(conv1_filter_size, conv1_stride_size, conv2_filter_size, conv2_str
 
     for _ in range(number_occurrences):
         X = layers.Conv2D(filters=conv1_num_filters, filter_size=(conv1_filter_size, conv1_filter_size), strides=(conv1_stride_size, conv1_stride_size), activation='relu')(X)
-        X = layers.SeparableConv2D(filters=conv2_num_filters, filter_size=(conv2_filter_size, conv2_filter_size), strides=(conv2_stride_size, conv2_stride_size), activation='relu')(X)
+        #X = layers.SeparableConv2D(filters=conv2_num_filters, filter_size=(conv2_filter_size, conv2_filter_size), strides=(conv2_stride_size, conv2_stride_size), activation='relu')(X)
         X = layers.MaxPooling2D(pool_size=max_pooling_filter_size)(X)
 
     X = layers.Flatten()(X)
