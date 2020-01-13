8dd407c210b9895339cf9a2f8574f51305670909
shabbeersh <36790230+shabbeersh@users.noreply.github.com>
2020-01-13 10:00:12 +0530

    Update AutoTune_ResNet50_Caltech101_Bayesian_v4.py

--- New/CalTech101/Bayesian Search/AutoTune_ResNet50_Caltech101_Bayesian_v4.py
+++ New/CalTech101/Bayesian Search/AutoTune_ResNet50_Caltech101_Bayesian_v4.py
@@ -25,7 +25,7 @@ TRAIN_PATH = os.path.join(DATA_FOLDER, "training") # Path for training data
 VALID_PATH = os.path.join(DATA_FOLDER, "validation") # Path for validation data
 NUMBER_OF_CLASSES = len(os.listdir(TRAIN_PATH)) # Number of classes of the dataset
 EPOCHS = 50
-RESULTS_PATH = os.path.join("AutoConv_ResNet50_new", "AutoFCL_AutoConv_ResNet50_log_" + DATA_FOLDER.split('/')[-1] + "_autoconv_bayes_opt_v1.csv") # The path to the results file
+RESULTS_PATH = os.path.join("AutoConv_ResNet50_new1", "AutoConv_ResNet50_log_" + DATA_FOLDER.split('/')[-1] + "_autoconv_bayes_opt_v1.csv") # The path to the results file
 
 # Creating generators from training and validation data
 batch_size=8 # the mini-batch size to use for the dataset
